e5188340c2cfbdbd86b4158083808ca1f24ce90f
Kislay Kaushal <kaushalkislay2@gmail.com>
2023-10-31 19:39:50 +0530

    added some more functionality

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,7 +1,41 @@
 import React, { useState } from 'react'
 
+
 function ToDoList() {
     const [inputData,setInputData]=useState('');
+    const [itemLists,setItemList] =useState([]);
+
+    const handleSubmit = (e) => {
+        e.preventDefault(); //stop default action of form
+       
+             //Adding element to list.
+            if(!inputData){
+            }else{
+                setItemList([...itemLists,inputData]);
+                setInputData('');
+            }
+      }
+
+    
+
+    //Deleting an item from the list.
+    const deleteItem = (id) =>{
+        const updateList= itemLists.filter((elem,ind)=>{
+            return ind != id;
+        })
+        setItemList(updateList);
+    }
+
+    //Editing the added item.
+    const handleEdit = (id) =>{
+        const updateList= itemLists.filter((elem,ind)=>{
+            return ind != id;
+        })
+        setItemList(updateList);
+        const editItem=itemLists[id];
+        setInputData(editItem);
+        
+    }
   return (
     <>
     <div className='main-div'>
@@ -10,10 +44,23 @@ function ToDoList() {
                 <h1>ToDo List</h1>
             </div>
 
-            <div className='addItems'>
+            <form className='addItems' onSubmit={handleSubmit}>
                 <input type="text" placeholder="Add items..." value={inputData} onChange={(e)=>setInputData(e.target.value)} />
-                <button type="button">Go!</button>
-            </div>
+                <button type="submit" onClick={handleSubmit} >Go</button>
+            </form>
+            <ul className='listItems'>
+                {
+                    itemLists.map((elem,ind)=>{
+                        return(
+                            <li className='eachItems' key={ind}>
+                                <span className='tolist'>{elem}</span>
+                                <button onClick={()=>handleEdit(ind)}>Edit</button>
+                                <button onClick={()=> deleteItem(ind)}>Delete</button>
+                            </li>
+                        )   
+                    })
+                }
+            </ul>
         </div>
 
     </div>
